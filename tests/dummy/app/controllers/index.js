4d3a74bd63f27141e679d6bf43192f321b3370fc
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-08-27 16:14:14 -0700

    adding starting index attribute

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,27 +1,43 @@
 import Ember from 'ember';
 import computed from 'ember-new-computed';
 
+function coerceSet(attributeName) {
+  return function(value) {
+    const schedulerName = attributeName + '_timer';
+
+    if (schedulerName) {
+      Ember.run.cancel(this[schedulerName]);
+    }
+
+    this[schedulerName] = Ember.run.later(() => {
+      const newValue = parseInt(value, 10);
+      if (!Number.isNaN(newValue)) {
+        this.set(attributeName, newValue);
+      }
+    }, 100);
+  };
+}
+
 export default Ember.Controller.extend({
   actions: {
     selected(modelType) {
       this.set('selected', modelType);
     },
     grow() {
-      this.set('height', this.get('height') + this.get('height') * .3);
+      this.set('height', this.get('height') + this.get('height') * 0.3);
     },
     shrink() {
-      this.set('height', this.get('height') - this.get('height') * .3);
+      this.set('height', this.get('height') - this.get('height') * 0.3);
     },
-    changeTimeout() {
-      const newValue = parseInt(this.get('scrollTimeout'), 10);
-      if (!Number.isNaN(newValue)) {
-        this.set('scrollTimeout', newValue);
-      }
-    }
+    setScrollTimeout: coerceSet('scrollTimeout'),
+    setPositionIndex: coerceSet('positionIndex')
   },
+
+  positionIndex: null,
   height: 200,
   scrollTimeout: null,
   selected: 'people',
+
   componentName: computed('selected', {
     get() {
       return 'x-' + this.get('selected');
