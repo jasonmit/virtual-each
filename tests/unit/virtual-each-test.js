e572c8fd4c9c9716a24331502064e6764f44c409
Alex Ford <alexford@frontside.io>
2015-10-30 12:19:02 -0500

    Add positioning tests
    Position to first item in the list should present the first item at the
    top of the list.
    Position to last item in the list should present the last item in the
    bottom of the list.
    
    Scrolling integration test do not currently work

---
+++ tests/unit/virtual-each-test.js
@@ -0,0 +1,177 @@
+/*global it, xdescribe, beforeEach */
+/*jshint -W030 */ // Expected an assignment or function call and instead saw an expression
+import '../test-helper';
+
+import Ember from 'ember';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+import { describe } from 'mocha';
+import { expect } from 'chai';
+import hbs from 'htmlbars-inline-precompile';
+
+const { run } = Ember;
+const EXTRA_ROW_PADDING = 1;
+
+describeComponent('virtual-each', 'VirtualEachComponent', {
+  // specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+  integration: true
+}, function() {
+
+  it('renders', function() {
+    // creates the component instance
+    this.render(hbs`
+      {{virtual-each}}
+    `);
+    var $component = this.$('.virtual-each');
+    expect($component.hasClass('virtual-each')).to.be.true;
+  });
+
+  describe("passing attributes", function() {
+
+    beforeEach(function() {
+      var items = new Array(200).fill(0).map((value, index)=> { return "Item ".concat(index); });
+      this.set('items', items);
+      this.render(hbs`
+        {{#virtual-each
+          items=items
+          positionIndex=positionIndex
+          height=500
+          itemHeight=35
+        as |item actualIndex virtualIndex|}}
+        <div style='height: 35px;'>{{item}} - actual {{actualIndex}} - virtual {{virtualIndex}}</div>
+        {{/virtual-each}}
+      `);
+      this.$component = this.$('.virtual-each');
+    });
+
+    it("styles the panel height", function() {
+      var obj = {};
+      var split = this.$component.attr('style').split(/[:;]/);
+      for (var i = 0; i < split.length-1; i+=2) {
+        obj[split[i].trim()] = split[i+1].trim();
+      }
+      expect(obj['height']).to.equal('500px');
+    });
+
+    it("styles the content height", function() {
+      var obj = {};
+      var split = this.$component.find('.infinite-list-content').attr('style').split(/[:;]/);
+      for (var i = 0; i < split.length-1; i+=2) {
+        obj[split[i].trim()] = split[i+1].trim();
+      }
+      var contentHeight = 35 * 200;
+      expect(obj['height']).to.equal(`${contentHeight}px`);
+    });
+
+    it("shows visible items in the panel", function() {
+      var visibleItems = this.$component.find('li');
+      var expectedLength = Math.ceil(500/35 + EXTRA_ROW_PADDING);
+      expect(visibleItems.length).to.equal(expectedLength);
+    });
+
+    it("first item is item 0", function () {
+      expect(this.$component.find('li').first().text()).to.contain('Item 0');
+      expect(this.$component.find('li').first().text()).to.contain('actual 0');
+      expect(this.$component.find('li').first().text()).to.contain('virtual 0');
+    });
+
+    describe("scrolling", function() {
+      beforeEach(function() {
+        this.itemHeight = 35;
+      });
+
+      it("scrolls to the bottom of the first item", function() {
+
+        let changedActionCallCount = 0;
+        var $component = this.$('.virtual-each');
+
+        $component.on('scroll', function() {
+          changedActionCallCount++;
+        });
+
+        run(() => {
+          this.$('.virtual-each').scrollTop(this.itemHeight);
+          this.$('.virtual-each').trigger('scroll');
+        });
+
+        expect(this.$component.find('li').first().text()).to.contain('Item 0');
+        expect(changedActionCallCount).to.equal(1);
+      });
+
+      xdescribe("scrolling to the top of the second item", function() {
+        beforeEach(function() {
+          run(() => {
+            this.$('.virtual-each').scrollTop(this.itemHeight + 1);
+            this.$('.virtual-each').trigger('scroll');
+          });
+        });
+
+        it("rerenders with the second item at the top", function() {
+          var $component = this.$('.virtual-each');
+          expect($component.find('li').first().text()).to.contain('Item 1');
+        });
+      });
+      describe("positioning to the second item", function() {
+        beforeEach(function() {
+          run(() => {
+            this.set('positionIndex', 1);
+          });
+        });
+
+        it("rerenders with the second item at the top", function() {
+          var $component = this.$('.virtual-each');
+          expect($component.find('li').first().text()).to.contain('Item 1');
+        });
+      });
+
+      describe("positioning to the last item in list", function() {
+        beforeEach(function() {
+          run(() => {
+            this.set('positionIndex', 199);
+          });
+        });
+
+        it("is the last item in the list", function() {
+          var $component = this.$('.virtual-each');
+          expect($component.find('li').last().text()).to.contain('Item 199');
+        });
+
+        it("sets the scrollTop such that the last item exists is the last visible item", function() {
+          var $component = this.$('.virtual-each');
+          let totalHeight = 200*35;
+          let virtualHeight = 500;
+
+          let expectedScrollTop = totalHeight - virtualHeight;
+          let halfItemHeight = 0.5 * 35;
+          expect($component.scrollTop()).to.be.closeTo(expectedScrollTop, halfItemHeight);
+        });
+      });
+      describe("positioning past the last item in list", function() {
+        beforeEach(function() {
+          run(() => {
+            this.set('positionIndex', 200);
+          });
+        });
+
+        it("does not position past the last item", function() {
+          var $component = this.$('.virtual-each');
+          expect($component.find('li').last().text()).to.contain('Item 199');
+        });
+
+        it("sets the scrollTop such that the last item exists is the last visible item", function() {
+          var $component = this.$('.virtual-each');
+          let totalHeight = 200*35;
+          let virtualHeight = 500;
+
+          let expectedScrollTop = totalHeight - virtualHeight;
+          let halfItemHeight = 0.5 * 35;
+          expect($component.scrollTop()).to.be.closeTo(expectedScrollTop, halfItemHeight);
+        });
+      });
+    });
+
+  });
+});
