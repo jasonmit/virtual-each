50f774394b022a24c7e19c12bc38f797b861ae2c
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-02-25 19:02:57 -0800

    Upgrading all dependencies

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,6 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.baseURL = '/virtual-each/';
     ENV.baseURL = '/virtual-each';
   }
 
