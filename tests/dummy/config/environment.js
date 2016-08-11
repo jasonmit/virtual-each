ef4e170d70582d41bf04e1bf7f4c791f8ff61e06
Jason Mitchell <jason.mitchell.w@gmail.com>
2016-08-10 21:28:10 -0700

    Upgrading deps

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -29,7 +29,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -41,8 +41,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.baseURL = '/virtual-each/';
-    ENV.baseURL = '/virtual-each';
+    ENV.rootURL = '/virtual-each/';
   }
 
   return ENV;
